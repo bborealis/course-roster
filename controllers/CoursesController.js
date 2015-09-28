aa8ff09dd1b7c7747840d7e733e2ddccd0a0409b
Brian Borealis & Chris Swan <student@epicodus.com>
2015-09-28 14:40:22 -0700

    add alert, add debug.

--- controllers/CoursesController.js
+++ controllers/CoursesController.js
@@ -1,4 +1,8 @@
 courseRoster.controller('CoursesCtrl', function CoursesCtrl($scope, CoursesFactory) {
   $scope.courses = CoursesFactory.courses;
   $scope.CoursesFactory = CoursesFactory;
+  $scope.ctrlCourse = "Math";
+  $scope.alarm = function() {
+    alert("alert alert!")
+  };
 });
--- directives/myDirectives.js
+++ directives/myDirectives.js
@@ -14,10 +14,34 @@ courseRoster.directive("changeClass", function() {
 
 courseRoster.directive("sampleCourse",function() {
   return {
-    scope:{
-        courseName:"@"
+    scope: {
+      courseName:"&"
     },
     template: '<div>{{courseName}}</div>',
+    }
 
+});
+
+courseRoster.directive("alertUser", function() {
+  return {
+    scope: {
+      alarm:"&"
+    },
+      template: '<div class="button" ng-click="alarm()">alarm!</div>'
+    }
+
+});
+
+courseRoster.directive("debug", function($compile) {
+  return {
+      terminal: true,
+      priority: 100000,
+      link: function (scope, element) {
+        var clone = element.clone();
+        element.attr("style", "color:red");
+        clone.removeAttr("debug");
+        var clonedElement = $compile(clone)(scope);
+        element.after(clonedElement);
+      }
   }
 });
