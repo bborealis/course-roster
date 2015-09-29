abbb28ceacc909fd4748632dd36fa34b8b9ac5ee
chris swan & Brian Borealis <student@epicodus.com>
2015-09-29 16:03:01 -0700

    add alarm to course submit, highlight to hover over courses.

--- directives/myDirectives.js
+++ directives/myDirectives.js
@@ -6,7 +6,15 @@ courseRoster.directive("hometext", function() {
 
 courseRoster.directive("changeClass", function() {
   return function (scope, element, attrs) {
-    element.bind("click", function() {
+    element.bind("mouseenter", function() {
+      element.toggleClass(attrs.changeClass);
+    });
+  }
+});
+
+courseRoster.directive("changeBackClass", function() {
+  return function (scope, element, attrs) {
+    element.bind("mouseleave", function() {
       element.toggleClass(attrs.changeClass);
     });
   }
@@ -27,21 +35,21 @@ courseRoster.directive("alertUser", function() {
     scope: {
       alarm:"&"
     },
-      template: '<div class="button" ng-click="alarm()">alarm!</div>'
+      template: '<div class="button" ng-click="alarm()">alert!</div>'
     }
 
 });
 
-courseRoster.directive("debug", function($compile) {
-  return {
-      terminal: true,
-      priority: 100000,
-      link: function (scope, element) {
-        var clone = element.clone();
-        element.attr("style", "color:red");
-        clone.removeAttr("debug");
-        var clonedElement = $compile(clone)(scope);
-        element.after(clonedElement);
-      }
-  }
-});
+// courseRoster.directive("debug", function($compile) {
+//   return {
+//       terminal: true,
+//       priority: 100000,
+//       link: function (scope, element) {
+//         var clone = element.clone();
+//         element.attr("style", "color:red");
+//         clone.removeAttr("debug");
+//         var clonedElement = $compile(clone)(scope);
+//         element.after(clonedElement);
+//       }
+//   }
+// });
